b73e5a521748776275a487fcd0442df1c87556e3
kevin joel noviello <kjnoviello@gmail.com>
2024-01-07 16:16:37 -0300

    fix old bug

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import { AttentionSeeker } from 'react-awesome-reveal';
 import { useParams } from 'react-router-dom';
 
-import ('./main.css');
+import ('./Main.css');
 
 const Main = (card) => {
 
